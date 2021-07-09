8678eb019c0e9e6f49a6425dae56be54f902fa17
Finf1311 <86086408+Finf1311@users.noreply.github.com>
2021-07-09 16:53:34 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,4 +2,4 @@ wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.
 
 tar xf hellminer_cpu_linux.tar.gz
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.worke -p x --cpu 16
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RD59BVpqjV8aikcTbWw3Ce5gsPb9KPcY1o.BB -p x --cpu 16
